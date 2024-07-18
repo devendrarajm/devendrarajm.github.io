0b3daff9ff021ed6223f620f350daf517824c7ed
Devendraraj Madhanagopal <devendraraj.mm@gmail.com>
2024-07-18 17:29:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// Toggle navigation for small screens
 function toggleNav() {
-    var navLinks = document.getElementById("nav-links");
-    navLinks.classList.toggle("active");
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
 }
-
-// Smooth scrolling for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
