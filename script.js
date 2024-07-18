5882284a9ce045f61f7667201072a8ec96c327cf
Devendraraj Madhanagopal <devendraraj.mm@gmail.com>
2024-07-18 15:01:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const navLinks = document.querySelectorAll('.nav-link');
-  const sections = document.querySelectorAll('.section');
-  const toggleButton = document.querySelector('.toggle-button');
-  const navLinksContainer = document.querySelector('.navbar-nav');
+    const navToggle = document.getElementById('nav-toggle');
+    const navTabs = document.getElementById('nav-tabs');
 
-  navLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-      event.preventDefault();
-      const target = this.getAttribute('data-target');
-
-      sections.forEach(section => section.classList.remove('active'));
-      navLinks.forEach(link => link.classList.remove('active'));
-
-      this.classList.add('active');
-      document.getElementById(target).classList.add('active');
+    navToggle.addEventListener('click', function() {
+        navTabs.classList.toggle('active');
     });
-  });
-
-  toggleButton.addEventListener('click', function() {
-    navLinksContainer.classList.toggle('active');
-    toggleButton.classList.toggle('active');
-  });
 });
