e099e8b0c8b31e99f224bcfb3e293ac15ed860c1
Devendraraj Madhanagopal <devendraraj.mm@gmail.com>
2024-07-18 21:20:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleNav() {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('active');
 }
+
+// Close the mobile menu when a link is clicked
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        const navLinks = document.getElementById('nav-links');
+        if (navLinks.classList.contains('active')) {
+            navLinks.classList.remove('active');
+        }
+    });
+});
