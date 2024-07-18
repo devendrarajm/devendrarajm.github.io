e2eb179cfcf0aadf03108f89f8adf533bcce5450
Devendraraj Madhanagopal <devendraraj.mm@gmail.com>
2024-07-18 11:48:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const currentPage = window.location.pathname.split("/").pop();
     const navLinks = document.querySelectorAll('.nav-link');
-    
+    const sections = document.querySelectorAll('.section');
+
     navLinks.forEach(link => {
-        if (link.getAttribute('href') === currentPage) {
-            link.classList.add('active');
-        } else {
-            link.classList.remove('active');
-        }
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const target = this.getAttribute('data-target');
+
+            // Remove active class from all sections and nav links
+            sections.forEach(section => section.classList.remove('active'));
+            navLinks.forEach(link => link.classList.remove('active'));
+
+            // Add active class to the clicked nav link and the corresponding section
+            this.classList.add('active');
+            document.getElementById(target).classList.add('active');
+        });
     });
 });
