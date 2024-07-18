b4d5f35fa9fe376d3e790510b28ab362f6436461
Devendraraj Madhanagopal <devendraraj.mm@gmail.com>
2024-07-18 11:45:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentPage = window.location.pathname.split("/").pop();
+    const navLinks = document.querySelectorAll('.nav-link');
+    
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+});
